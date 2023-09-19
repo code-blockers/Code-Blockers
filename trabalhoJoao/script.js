123bd7d6770a11f57becd28c359ebf79c7261940
Lucas Queiroz <lucas.vrq07@gmail.com>
2023-09-18 22:20:03 -0300

    organizando pastas v2
    Organizando Pastas.

--- trabalhoJoao/script.js
+++
@@ -1,7 +0,0 @@
-const buyButtons = document.querySelectorAll(".buy-button");
-buyButtons.forEach(button => {
-  button.addEventListener("click", (event) => {
-    const productId = event.target.getAttribute("data-product");
-    alert(`Você comprou o produto ${productId}!`);
-  });
-});
