5189646af16535ac263996a11be5a2c572f4287a
Roccio7 <131801947+Roccio7@users.noreply.github.com>
2023-11-27 00:45:49 -0300

    Delete CodeBlockers/insert-produtos3.php

--- CodeBlockers/insert-produtos3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexao = mysqli_connect("localhost", "root", "", "7ssoqueedaora");
-$var = 'SELECT * FROM produto WHERE modelo = "infantil"'; 
-$query = mysqli_query($conexao,$var);
-
-//while(){}
-//foreach {}
-//for
-?>
